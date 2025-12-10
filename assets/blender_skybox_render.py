b7a88974ee76bb608eea6195fcd9b5d01e9e847b
spacehare <dantath425@gmail.com>
2025-12-10 01:03:27 -0500

    Create blender_skybox_render.py

---
+++ assets/blender_skybox_render.py
@@ -0,0 +1,53 @@
+import math
+from pathlib import Path
+
+import bpy
+
+# lots of this code was just yoinked from my Spyro skyboxes project
+# https://github.com/spacehare/spyro-blender/tree/main
+
+rotations = {
+    "ft": (90, 0, 0),  # +Y
+    "rt": (90, 0, 90),  # +X
+    "bk": (90, 0, 180),  # -Y
+    "lf": (90, 0, 270),  # -X
+    "up": (180, 0, 90),  # +Z
+    "dn": (0, 0, 90),  # -Z
+}
+scene_camera = bpy.context.scene.camera
+output_path = Path.home() / Path(r"example/stem")
+resolution = 1024
+
+
+def setup():
+    bpy.context.scene.render.image_settings.file_format = "TARGA"
+    camera = bpy.context.scene.camera
+    camera.data.lens = 18  # 90 FOV
+    camera.data.clip_end = 10000
+    camera.hide_select = True
+
+
+def rotate(what, vector):
+    what.rotation_euler.x = math.radians(vector[0])
+    what.rotation_euler.z = math.radians(vector[1])
+    what.rotation_euler.y = math.radians(vector[2])
+
+
+def render(output_file_path: Path, xy: int):
+    bpy.context.scene.render.resolution_x = xy
+    bpy.context.scene.render.resolution_y = xy
+    bpy.context.scene.render.filepath = str(output_file_path)
+    bpy.ops.render.render(write_still=True)
+
+
+def render_skybox(file_path: Path, camera, resolution: int):
+    print(f"{resolution}x{resolution}", file_path)
+
+    for direction, vector in rotations.items():
+        item_path = file_path.with_stem(f"{file_path.stem}_{direction}")
+        rotate(camera, vector)
+        render(item_path, resolution)
+
+
+setup()
+render_skybox(output_path, scene_camera, resolution)
